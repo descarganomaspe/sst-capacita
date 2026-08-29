32c2bc0a3a8ed29bc4bc5f6d9d259ff567510211
descarganomaspe <marcelolamasa@gmail.com>
2026-08-29 15:16:59 -0500

    El cupo del plan se cuenta por empresa, no por celular

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sstc-39e8d3078c';
+var CACHE = 'sstc-2b0a5493c0';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
