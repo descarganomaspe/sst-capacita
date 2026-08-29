0fc73c37284dde9e379554f338b011cd86fb3563
descarganomaspe <marcelolamasa@gmail.com>
2026-08-28 19:59:39 -0500

    Arregla la activacion de codigos + planes Prevencionista, Gestion y prueba de 30 dias
    La app llamaba sst_activar_licencia con p_codigo pero la funcion se llamaba p_hash: PostgREST nunca la encontro y ningun codigo se pudo canjear (0 de 340). Ahora la funcion recibe p_codigo, limpia el codigo y busca su sha256. Se anaden los prefijos PRV, GES y PRU, y el mensaje de activacion nombra el plan real y avisa cuando es prueba gratis.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sstc-cda6149ee1';
+var CACHE = 'sstc-316ee51d1c';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
