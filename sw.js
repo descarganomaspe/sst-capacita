8d53ce896e2fa74eaea0908f61dd02eccf1531a5
descarganomaspe <marcelolamasa@gmail.com>
2026-08-30 14:25:06 -0500

    Inspecciones tecnicas por sector: 57 formatos y el levantamiento de observaciones

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sstc-b1e78855c7';
+var CACHE = 'sstc-4ef7de33b8';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
