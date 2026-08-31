c8bb1f3a6ffe2fa2e91ceb35b0b5b929f3e22b94
descarganomaspe <marcelolamasa@gmail.com>
2026-08-31 06:56:09 -0500

    baf261100d - errores en cristiano, Volver al inicio por rol, botones con nombre

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sstc-73c8a075a2';
+var CACHE = 'sstc-baf261100d';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
