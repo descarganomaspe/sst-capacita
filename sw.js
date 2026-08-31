f6cbae3b61c2af93b45ce5d43353ed85563b74e3
descarganomaspe <marcelolamasa@gmail.com>
2026-08-30 20:17:17 -0500

    Arreglo: la pantalla del plan Gestion ya no deja preso al supervisor (75bb762e0d)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sstc-c8cec2c16b';
+var CACHE = 'sstc-75bb762e0d';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
