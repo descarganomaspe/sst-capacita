4d51f78089b02cd7f30a74ff0a651b40d6cfb420
descarganomaspe <marcelolamasa@gmail.com>
2026-08-31 19:14:06 -0500

    889c1015ee - personal sincronizado, QR abre OBRASST, carne en PDF, actividad en el kardex, capacitaciones por plan

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sstc-baf261100d';
+var CACHE = 'sstc-889c1015ee';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
