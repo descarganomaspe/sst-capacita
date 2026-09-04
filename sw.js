1c6a985894c864e9d6b9e83798792fc7b44a35ef
descarganomaspe <marcelolamasa@gmail.com>
2026-09-04 06:35:40 -0500

    Auditoria de los PDF: se corrigen el 011-2019-TR y el 023-2017-EM, se suma el CNE Utilizacion completo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sstc-0732f69ef6';
+var CACHE = 'sstc-2458566ad6';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
