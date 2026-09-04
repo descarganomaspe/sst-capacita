caf75eef28c35e9093eda42223846c03dd1c403b
descarganomaspe <marcelolamasa@gmail.com>
2026-09-03 21:48:47 -0500

    Bienvenida inicial, pack EPP hombre nuevo y charlas por actividad del supervisor

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sstc-dd146e69f2';
+var CACHE = 'sstc-e9ee3fc2d4';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
