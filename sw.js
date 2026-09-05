7d0dc7666f35ade12f5a956600850f73025d2af2
descarganomaspe <marcelolamasa@gmail.com>
2026-09-04 23:03:34 -0500

    Plantillas limpias (fuera la imagen con datos personales y el nombre de la constructora ajena), firmas dentro del Excel, registro del mes armado desde el kardex, y la cola de subida con respuesta y comprobacion contra el servidor

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sstc-a648c5be3c-b';
+var CACHE = 'sstc-b8f162c022-b';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
