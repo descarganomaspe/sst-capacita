fdc2d9da6f973e8ac21524cfaccd1b4ae8f7638b
descarganomaspe <marcelolamasa@gmail.com>
2026-09-04 23:48:42 -0500

    Papeleta: PDF de una sola hoja A4, queda en el legajo del trabajador sin preguntar, sale en su ficha, y base normativa buscada dentro de las normas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sstc-4395dbed20-b';
+var CACHE = 'sstc-77d7aa3689-b';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
