29461c779e9341c2567ace0df197da48ae032754
descarganomaspe <marcelolamasa@gmail.com>
2026-09-06 21:24:54 -0500

    4 temas Metro (RISST, EPC, emergencias, Comite) + trabajador del mes con diploma + QR profesional + Torre: lo que se mueve primero + base normativa en todas las normas + ids duplicados

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sstc-328b4b7764-b';
+var CACHE = 'sstc-9404e4efca-b';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
