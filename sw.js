5ec48330bd99f4a6948a09e7897949aef8cc57c0
descarganomaspe <marcelolamasa@gmail.com>
2026-09-06 22:51:40 -0500

    Constancia del trabajador como documento (logo, nota grande, QR de folio) + copia del empleador igual + firma grande + sello del supervisor + kardex 13 filas por hoja

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sstc-2691b3a418-b';
+var CACHE = 'sstc-e76dc6cd8c-b';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){
   e.waitUntil(caches.keys().then(function(ks){
