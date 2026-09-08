4c1e30527f16e09e85d2cd119e99206ed2bfb007
descarganomaspe <marcelolamasa@gmail.com>
2026-09-07 23:54:42 -0500

    Talleres con ilustraciones de verdad en vez de dibujos de linea (sello ab1daa0223)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sstc-797d5ac073-c';
+var CACHE = 'sstc-ab1daa0223-c';
 /* ══ LO QUE SE GUARDA AL INSTALAR ══
    Antes install no guardaba nada: el SW viejo servia el index nuevo por
    red, lo metia en el cache VIEJO, y al activarse el SW nuevo borraba ese
