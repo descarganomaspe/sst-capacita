7a8ee9a606c29bff188a2bab4d4294b01768f47d
descarganomaspe <marcelolamasa@gmail.com>
2026-09-08 06:55:25 -0500

    Una fotografia de obra por cada una de las 49 capacitaciones, con circulos y flechas sobre lo que hay que mirar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sstc-09bba04f6b-c';
+var CACHE = 'sstc-ff36ad65a3-c';
 /* ══ LO QUE SE GUARDA AL INSTALAR ══
    Antes install no guardaba nada: el SW viejo servia el index nuevo por
    red, lo metia en el cache VIEJO, y al activarse el SW nuevo borraba ese
