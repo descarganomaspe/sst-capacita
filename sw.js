7f3a7a10ef78ee470de1d0e414e59e1bc21eb21f
descarganomaspe <marcelolamasa@gmail.com>
2026-09-08 00:59:17 -0500

    Presentaciones de capacitacion con fotografias de obra subterranea y flechas que senalan lo importante

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'sstc-ab1daa0223-c';
+var CACHE = 'sstc-09bba04f6b-c';
 /* ══ LO QUE SE GUARDA AL INSTALAR ══
    Antes install no guardaba nada: el SW viejo servia el index nuevo por
    red, lo metia en el cache VIEJO, y al activarse el SW nuevo borraba ese
